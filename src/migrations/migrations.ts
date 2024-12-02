60de654700ae9b983c9523097ed2c496d17716c4
Carlos-Andrad <carloseduardoandradesilva03@outlook.com>
2024-12-02 12:40:26 +0000

    Adicionando migrations

---
+++ src/migrations/migrations.ts
@@ -0,0 +1,45 @@
+import pool from '../config/database';
+
+const createUsersTable = async () => {
+  const client = await pool.connect();
+  try {
+    const queryText = `
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL
+        senha VARCHAR(100) NOT NULL
+      )
+      );
+    `;
+    await client.query(queryText);
+    console.log('Tabela "users" criada com sucesso!');
+  } catch (err) {
+    console.error('Erro ao criar tabela:', err);
+  } finally {
+    client.release();
+  }
+};
+
+const createContatosTable = async () => {
+  const contato = await pool.connect();
+  try {
+    const queryText = `
+      CREATE TABLE IF NOT EXISTS contatos (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        telefone VARCHAR(100) NOT NULL,
+        image VARCHAR(100) NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL
+      );
+    `;
+    await contato.query(queryText);
+    console.log('Tabela "contatos" criada com sucesso!');
+  } catch (err) {
+    console.error('Erro ao criar tabela:', err);
+  } finally {
+    contato.release();
+  }
+};
+
+createContatosTable().then(() => process.exit(0));
